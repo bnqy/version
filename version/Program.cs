d30b18dda4e2468fbe208d8ef204c4a566b720b6
Beneqi <obbonobo@gmail.com>
2023-08-25 22:01:01 +0600

    Count types and methods

--- version/Program.cs
+++ version/Program.cs
@@ -2,4 +2,25 @@
 
 using System.Reflection;
 
-Console.WriteLine("Hello, World!");
+
+Assembly? assembly =  Assembly.GetEntryAssembly();
+
+if (assembly == null)
+    return;
+
+foreach (AssemblyName name in assembly.GetReferencedAssemblies())
+{
+    Assembly a = Assembly.Load(name);
+
+    int methodCount = 0;
+    
+    foreach (TypeInfo t in a.DefinedTypes)
+    {
+        methodCount += t.GetMethods().Count();
+    }
+    
+    Console.WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.",
+    arg0: a.DefinedTypes.Count(),
+    arg1: methodCount, 
+    arg2: name.Name);
+}
