f801ff72b06d166beb04e3fae97056aee744516c
Beneqi <obbonobo@gmail.com>
2023-08-27 18:35:07 +0600

    Bit

---
+++ Bit Logic/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+using static System.Convert;
+
+static string ToBinString(int i)
+{
+    return Convert.ToString(i, toBase: 2).PadLeft(8, '0');
+}
+
+WriteLine(ToBinString(3));
+
+
+WriteLine(int.MaxValue);
+
+double d = 9.8;
+int i = (int)d;
+
+WriteLine(i);
+
+i = ToInt32(d);
+WriteLine(i);
