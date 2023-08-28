6294f1bbbd075e485f9fc71e1f16a7c16a7b46be
Beneqi <obbonobo@gmail.com>
2023-08-28 22:57:50 +0600

    First

---
+++ Practice03/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+WriteLine("Input a number between 0 and 255: ");
+string? firstStr = ReadLine();
+
+WriteLine("Input a number between 0 and 255: ");
+string? secondStr = ReadLine();
+
+if (Int32.TryParse(firstStr, out int i) & Int32.TryParse(secondStr, out int j))
+{
+    WriteLine($"{i}/{j} is {i/j}");
+}
