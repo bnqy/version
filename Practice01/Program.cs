78a2aee061d7f04eb76bafc604e5135b7c9669e8
Beneqi <obbonobo@gmail.com>
2023-08-28 22:34:04 +0600

    Checking Over Flow Ex

---
+++ Practice01/Program.cs
@@ -0,0 +1,15 @@
+﻿int max = 500;
+try
+{
+    checked
+    {
+        for (byte i = 0; i < max; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.Write($"Type: {ex.GetType()} - {ex.Message}");
+}
