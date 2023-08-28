e8c01c6e9e974a309bd55a01f002066598c0b094
Beneqi <obbonobo@gmail.com>
2023-08-28 11:40:26 +0600

    Ex Get Type

---
+++ Exception/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+using static System.Convert;
+
+WriteLine("Before parsing");
+Write("What is your age? ");
+string? input = ReadLine();
+try
+{
+    int age = int.Parse(input);
+    WriteLine($"You are {age} years old.");
+}
+catch(Exception ex )
+{
+    WriteLine($"{ex.GetType()} says {ex.Message}");
+}
+WriteLine("After parsing");
