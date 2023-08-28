58e152d6e7a15af565098d8979e816044e98d27f
Beneqi <obbonobo@gmail.com>
2023-08-28 22:45:32 +0600

    First Solution

---
+++ Practice02/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+for(int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Write("FizzBuzz ");
+    }
+    else if (i % 3 == 0)
+    {
+        Write("Fizz ");
+        continue;
+    }
+    else if (i % 5 == 0)
+    {
+        Write("Buzz ");
+    }
+    else
+        Write($"{i} ");
+}
