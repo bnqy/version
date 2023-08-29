f9180024fcdc740e6982b14e9b67aff72c72e29b
Beneqi <obbonobo@gmail.com>
2023-08-29 11:12:23 +0600

    Static

---
+++ Methods/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+Table(7);
+
+static void Table(byte b)
+{
+    WriteLine($"{b}'s time table:");
+
+    for(byte b2 = 1; b2 <= 10; b2++)
+    {
+        WriteLine($"{b} x {b2} = {b * b2}");
+    }
+}
