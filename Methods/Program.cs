1e631e3f6e745ea80e101145e6b24615f3326e52
Beneqi <obbonobo@gmail.com>
2023-08-29 11:38:54 +0600

    CardToOrd

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,6 +1,8 @@
 ﻿using static System.Console;
 
-Table(7);
+Table(9);
+WriteLine();
+Write(CardinalToOrdinal(123));
 
 static void Table(byte b)
 {
@@ -9,5 +11,29 @@ static void Table(byte b)
     for(byte b2 = 1; b2 <= 10; b2++)
     {
         WriteLine($"{b} x {b2} = {b * b2}");
+    }
+}
+
+static string CardinalToOrdinal(int number)
+{
+    int lastTwoDigit = number % 100;
+
+    switch (lastTwoDigit)
+    {
+        case 11:
+        case 12:
+        case 13:
+            return $"{number}th";
+        default:
+            int lastDigit = lastTwoDigit % 10;
+            string last = lastDigit switch
+            {
+                1 => "st",
+                2 => "nd",
+                3 => "rd",
+                _ => "th"
+
+            };
+            return $"{number}{last}";
     }
 }
