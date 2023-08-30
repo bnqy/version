d34dcc3a4b4113c5717fd8de4869662c598f0d67
Beneqi <obbonobo@gmail.com>
2023-08-31 00:18:19 +0600

    Debugging

---
+++ Debugigi/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+static double Add(double d1, double d2)
+{
+    return d1 + d2;
+}
+
+double d1 = 10.0;
+double d2 = 3.14;
+double result = Add(d1, d2);
+WriteLine($"The sum result is {result}");
+WriteLine(result);
+WriteLine("Press ENTER to end the app.");
+ReadLine();
