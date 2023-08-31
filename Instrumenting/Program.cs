90e7906cf795b811be5d88d56b76ae2d653792c1
Beneqi <obbonobo@gmail.com>
2023-08-31 12:08:19 +0600

    Tracing & Debugging

---
+++ Instrumenting/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics;
+
+
+Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log.txt"))));
+
+Trace.AutoFlush = true;
+
+Debug.WriteLine("Debug, !");
+Trace.WriteLine("Trace, !");
