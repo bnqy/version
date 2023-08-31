f6684321b6fa3a8ec34811a1e42f57290e78e312
Beneqi <obbonobo@gmail.com>
2023-08-31 12:46:15 +0600

    Trace Levels {Off, Error, Warning, Info, Verbose}

--- Instrumenting/Program.cs
+++ Instrumenting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
 
 
 Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log.txt"))));
@@ -7,3 +8,17 @@ Trace.AutoFlush = true;
 
 Debug.WriteLine("Debug, !");
 Trace.WriteLine("Trace, !");
+
+ConfigurationBuilder builder = new();
+builder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+IConfigurationRoot configuration = builder.Build();
+
+TraceSwitch ts = new(
+ displayName: "PacktSwitch",
+ description: "This switch is set via a JSON config.");
+
+configuration.GetSection("PacktSwitch").Bind(ts);
+Trace.WriteLineIf(ts.TraceError, "Trace error");
+Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
