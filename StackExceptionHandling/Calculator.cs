25eff8d34a6ae90d8d6fb5970d200de6b681500a
Beneqi <obbonobo@gmail.com>
2023-08-31 19:53:25 +0600

    Stack Exception Handling

---
+++ StackExceptionHandling/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+namespace StackExceptionHandlingLib;
+
+public class Calculator
+{
+    public static void Gamma()
+    {
+        WriteLine("In Gamma");
+        Delta();
+    }
+    private static void Delta()
+    {
+        WriteLine("In Delta");
+        File.OpenText("bad file path");
+    }
+}
---
+++ StackExceptionHandlingApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using StackExceptionHandlingLib;
+using static System.Console;
+
+WriteLine("In Main");
+Alpha();
+static void Alpha()
+{
+    WriteLine("In Alpha");
+    Beta();
+}
+static void Beta()
+{
+    WriteLine("In Beta");
+    try
+    {
+        Calculator.Gamma();
+    }
+    catch (Exception ex)
+    {
+        WriteLine($"Caught this: {ex.Message}");
+
+        throw new Exception(message: "Dua Lipa", innerException: ex);
+    }
+}
