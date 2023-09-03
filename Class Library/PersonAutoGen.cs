19455e4336271e8bbefce5d0b6e08f6e821a378e
Beneqi <obbonobo@gmail.com>
2023-09-03 11:36:53 +0600

    Get Set

--- Class Library/PersonAutoGen.cs
+++ Class Library/PersonAutoGen.cs
@@ -13,5 +13,31 @@ namespace Class_Library_Shared
 
         public string Greetings => $"Greetings from {Name}";
         public int Age => DateTime.Today.Year - DateOfBirth.Year;
+
+        public string favPrimeColor;
+        public string FavPrimeColor
+        {
+            get
+            {
+                return favPrimeColor;
+            }
+
+            set
+            {
+                switch (value.ToLower())
+                {
+                    case "red":
+                    case "green":
+                    case "blue":
+                        favPrimeColor = value;
+                        break;
+                    default:
+                        throw new System.ArgumentException(
+                        $"{value} is not a primary color. " +
+                        "Choose from: red, green, blue.");
+                }
+            }
+        }
+
     }
 }
