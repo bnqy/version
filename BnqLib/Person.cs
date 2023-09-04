df91a82f6af79070167a009947e7f4db59d4f867
Beneqi <obbonobo@gmail.com>
2023-09-04 21:00:35 +0600

    New Class Lib

---
+++ BnqLib/Person.cs
@@ -0,0 +1,26 @@
+﻿namespace BnqLib;
+
+public class Person
+{
+    public string? Name;
+    public DateTime DateOfBirth;
+    public List<Person> Children = new();
+
+    public static Person BabyCreate(Person p1, Person p2)
+    {
+        Person baby = new()
+        {
+            Name = $"The baby of {p1.Name} and {p2.Name}"
+        };
+
+        p1.Children.Add(baby);
+        p2.Children.Add(baby);
+
+        return baby;
+    }
+
+    public Person BabyCreateWith(Person partner)
+    {
+        return BabyCreate(this, partner);
+    }
+}
---
+++ PeopleApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using BnqLib;
+using static System.Console;
+
+Person kylie = new Person() {Name = "Kylie" };
+Person travis = new() { Name = "Travis" };
+Person stormi = kylie.BabyCreateWith(travis);
+//WriteLine(strormi.Name);
+stormi.Name = "Stormi";
+Person air = Person.BabyCreate(kylie, travis);
+air.Name = "Air";
+
+WriteLine($"Kylie has {kylie.Children.Count} children");
+WriteLine($"Travis has \"{travis.Children.Count}\" children");
+WriteLine("Their children:");
+foreach (var children in kylie.Children)
+{
+    WriteLine($" {children.Name}");
+}
