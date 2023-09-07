83b201f7de8a1c5ff47279059017fe6121485592
Beneqi <obbonobo@gmail.com>
2023-09-07 10:31:56 +0600

    Default Impl In Interfaces

---
+++ BnqLib/DvdPlayer.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+namespace BnqLib
+{
+    public class DvdPlayer : IPlayable
+    {
+        public void Pause()
+        {
+            WriteLine($"Dvd player is pausing");
+        }
+
+
+        public void Play()
+        {
+            WriteLine($"Dvd player is playing");
+        }
+    }
+}
---
+++ BnqLib/IPlayable.cs
@@ -0,0 +1,12 @@
+﻿namespace BnqLib;
+
+public interface IPlayable
+{
+    void Play();
+    void Pause();
+
+    void Stop()
+    {
+        Console.WriteLine("Default impl of method");
+    }
+}
